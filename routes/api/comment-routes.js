56c12beba8ac2b894079310cbb5582b7e493017f
Shital Chaudhary <shitalchau10@gmail.com>
2021-10-17 15:40:03 -0500

    Update the Comment Controller and Routes

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { addComment, removeComment } = require('../../controllers/comment-controller');
+const { addComment, addReply, removeReply, removeComment } = require('../../controllers/comment-controller');
 
 // /api/comments/<pizzaId>
 
@@ -10,6 +10,13 @@ router
 // /api/comments/<pizzaId>/<commentId>
 router
     .route('/:pizzaId/:commentId')
+    .put(addReply)
     .delete(removeComment);
+
+router
+    .route('/:pizzaId/:commentId/:replyId')
+    .delete(removeReply);
+    
+
 
 module.exports = router;
